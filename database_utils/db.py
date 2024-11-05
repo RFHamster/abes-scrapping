45406150e9eef0cb88dd849799e514def0c8bd94
Rhuan Fernandes <rhuanfernandes@biofy.tech>
2024-11-05 12:31:02 -0300

    adding base of all graphs in api

--- database_utils/db.py
+++ database_utils/db.py
@@ -8,29 +8,56 @@ def connect_mongo_abes():
     return database['data']
 
 
-collection = connect_mongo_abes()
+collection_abes = connect_mongo_abes()
 
 
 def insert_data_abes(dict_data):
-    collection.insert_one(dict_data)
+    collection_abes.insert_one(dict_data)
 
 
 def insert_many_data_abes(many_data):
-    collection.insert_many(many_data)
+    collection_abes.insert_many(many_data)
 
 
 def get_dict_abes_scrapping():
-    return list(collection.find({}, {'_id': 0}))
+    return list(collection_abes.find({}, {'_id': 0}))
+
+
+def get_dict_br_positions():
+    return list(
+        collection_abes.find(
+            {}, {'_id': 0, 'brazil_position': 1, 'study_year': 1}
+        )
+    )
 
 
 def get_dict_br_versus_la():
     return list(
-        collection.find(
-            {},
+        collection_abes.find(
+            {
+                'brazil_movement': {'$ne': 'Not Informed'},
+                'latin_america_investment': {'$ne': 'Not Informed'},
+            },
             {
                 '_id': 0,
                 'brazil_movement': 1,
                 'latin_america_investment': 1,
+                'study_year': 1,
+            },
+        )
+    )
+
+def get_dict_br_versus_world():
+    return list(
+        collection_abes.find(
+            {
+                'brazil_movement': {'$ne': 'Not Informed'},
+                'global_investment': {'$ne': 'Not Informed'},
+            },
+            {
+                '_id': 0,
+                'brazil_movement': 1,
+                'global_investment': 1,
                 'study_year': 1,
             },
         )
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from scrapping.script_abes import update_data_abes
-from database_utils.db import get_dict_abes_scrapping, get_dict_br_versus_la
+from database_utils.db import (
+    get_dict_abes_scrapping,
+    get_dict_br_versus_la,
+    get_dict_br_positions,
+    get_dict_br_versus_world
+)
 
 app = FastAPI()
 
@@ -17,12 +22,33 @@ def read_root():
 
 @app.get('/abes/table')
 def get_table_abes_scrapping():
+    """
+    Get all data from abes scrapping in the formate of HTML Table
+    """
     return get_dict_abes_scrapping()
 
 
 @app.get('/abes/pizza/br_la')
 def get_pizza_br_x_la():
+    """
+    Get Brazil and Latin America's investment values along the years in the format of Pizza Graph in ChartJS
+    """
     return get_dict_br_versus_la()
+
+@app.get('/abes/bars/br_world')
+def get_bars_br_x_world():
+    """
+    Get Brazil and World's investment values along the years in the format of Bars Graph in ChartJS
+    """
+    return get_dict_br_versus_world()
+
+
+@app.get('/abes/line/br_position')
+def get_line_br_position():
+    """
+    Get Brazil positions along the years in the format of Line Graph in ChartJS
+    """
+    return get_dict_br_positions()
 
 
 @app.post('/abes/ingest')
